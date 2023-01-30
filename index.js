0b1b4f99690c96608258236a1a8a9a8e2e99090b
Terry Kubalewski <terrykub@yahoo.com>
2023-01-30 14:50:34 -0600

    added auth routher

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
     res.render('register', { username: 'terryk' });
 });
 
+// Import the function from routes module
+const initRoutes = require('./src/routes')
+// Execute the function with app as the argument
+initRoutes(app);
+
 app.listen(port, () => {
-    console.log(`Servier is listening on port ${port}`)
+    console.log(`Server is listening on port ${port}`)
 })
