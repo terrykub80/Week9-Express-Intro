463a32c689f4731524c0e6266319a060daaede02
Terry Kubalewski <terrykub@yahoo.com>
2023-01-30 15:09:36 -0600

    update register route and views folder location

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const path = require('path');
 
 app.set('view engine', 'ejs');
+// Update the location of the folder for res.render views
+app.set('views', path.join(__dirname, 'src/templates/views'));
 
 app.get('/', (req, res) => {
-    res.render('register', { username: 'terryk' });
+    res.send('Hello World');
 });
 
 // Import the function from routes module
-const initRoutes = require('./src/routes')
+const initRoutes = require('./src/routes');
 // Execute the function with app as the argument
 initRoutes(app);
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
-})
+});
