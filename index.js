986ab28613605c9cd61acd261fbd8e495a37a537
Terry Kubalewski <terrykub@yahoo.com>
2023-01-31 11:54:11 -0600

    add mongodb and connect via mongoose

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+const dotenv = require('dotenv');
+dotenv.config();
+console.log(process.env);
 const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const { connectDB } = require('./src/db');
+
+// Execute the connectDB function to connect to our database
+connectDB();
 
 app.set('view engine', 'ejs');
 // Update the location of the folder for res.render views
@@ -13,6 +20,7 @@ app.use(express.urlencoded({extended: true}));
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
 
 // Import the function from routes module
 const initRoutes = require('./src/routes');
---
+++ src/db/index.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    mongoose.set('strictQuery', false)
+    const connection = await mongoose.connect(process.env.MONGO_URI);
+    console.log('mongoDB Connected!');
+};
+
+module.exports = { connectDB }
