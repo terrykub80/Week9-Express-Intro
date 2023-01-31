f1a51fbe54d4a0506f8aa849cb761dae50fd406a
Terry Kubalewski <terrykub@yahoo.com>
2023-01-31 13:57:54 -0600

    added bcrypt for password hashing

--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -1,14 +1,18 @@
 const { User } = require('../../models')
+const bcrypt = require('bcrypt');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     console.log(req.body);
     // First check to make sure the password and confirmPass are the same
     if (req.body.password != req.body.confirmPass){
         res.send({error: 'Your passwords do not match'})
-    };
-    // Get the data from the request body
-    const { username, email, password } = req.body
-    const user = new User({ username, email, password });
-    user.save();
-    res.send(`New User Created - ${user.username}`);
+    } else {
+        // Get the data from the request body
+        const { username, email, password } = req.body
+        // Has the password before creating user
+        const passwordHash = await bcrypt.hash(password, 10);
+        const user = new User({ username, email, password: passwordHash });
+        user.save();
+        res.send(`New User Created - ${user.username}`);
+    }
 };
