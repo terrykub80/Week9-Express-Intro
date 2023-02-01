4e4a5cbd77d6ab6133bad4c76f2352cce7cc2801
Terry Kubalewski <terrykub@yahoo.com>
2023-02-01 12:19:38 -0600

    Update the POST register route to make register mutation

--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -1,5 +1,4 @@
-const { User } = require('../../models')
-const bcrypt = require('bcrypt');
+const axios = require('axios');
 
 module.exports = async (req, res) => {
     console.log(req.body);
@@ -7,12 +6,42 @@ module.exports = async (req, res) => {
     if (req.body.password != req.body.confirmPass){
         res.send({error: 'Your passwords do not match'})
     } else {
-        // Get the data from the request body
-        const { username, email, password } = req.body
-        // Has the password before creating user
-        const passwordHash = await bcrypt.hash(password, 10);
-        const user = new User({ username, email, password: passwordHash });
-        user.save();
-        res.send(`New User Created - ${user.username}`);
+        // // Get the data from the request body
+        // const { username, email, password } = req.body
+        // // Has the password before creating user
+        // const passwordHash = await bcrypt.hash(password, 10);
+        // const user = new User({ username, email, password: passwordHash });
+        // user.save();
+        // res.send(`New User Created - ${user.username}`);
+        try {
+            const mutation = `
+            mutation ($email: String!, $username: String!, $password: String!){
+                register(email: $email, username: $username, password: $password)
+            }
+            `
+            const { data } = await axios.post(process.env.GRAPHQL_ENDPOINT, 
+                    {
+                        query: mutation, 
+                        variables: {
+                            email: req.body.email,
+                            username: req.body.username,
+                            password: req.body.password,
+                        }
+                    },
+                    {
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    }
+                )
+
+            const jwtToken = data.data.register
+            console.log(jwtToken);
+
+            res.redirect('/')
+        }catch(err){
+            console.log(err)
+            res.redirect('/auth/register')
+        }
     }
 };
