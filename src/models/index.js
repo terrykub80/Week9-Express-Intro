810aeae01ba12d610732dde1a5fbb1dbcd85ab9a
Terry Kubalewski <terrykub@yahoo.com>
2023-02-02 12:00:46 -0600

    fixed question model

--- src/models/index.js
+++ src/models/index.js
@@ -2,4 +2,4 @@ module.exports = {
     User: require('./user.model'),
     Quiz: require('./quiz.model'),
     Question: require('./question.model')
-}
+};
--- src/models/question.model.js
+++ src/models/question.model.js
@@ -23,4 +23,4 @@ const questionSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
-module.exports = new mongoose.model("Question", questionSchema)
+module.exports = mongoose.model("Question", questionSchema);
